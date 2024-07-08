e0d1d9afb9468b07c2e1ed7bb8410e7a501f1288
sfauzii <sjahrulfauzii@gmail.com>
2024-07-08 19:48:35 +0700

    refactor(all): updated design

--- frontend/scripts/dashboard.js
+++ frontend/scripts/dashboard.js
@@ -133,3 +133,27 @@ document.addEventListener("DOMContentLoaded", function() {
     // Membuat tombol-tombol paginasi
     createPaginationButtons();
 });
+
+// dropdown
+// JavaScript to toggle dropdown
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown_content");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.profile-image')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
