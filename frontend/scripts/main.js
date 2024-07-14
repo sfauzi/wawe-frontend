4742c08d97d50c7368f0ba86cfe87f2fe025865d
sfauzii <sjahrulfauzii@gmail.com>
2024-07-14 15:25:05 +0700

    refactor(index): update design in index

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -13,38 +13,38 @@
 // prevEl: '.swiper-button-prev',
 // },
 
-
 // });
 
-let prevScrollpos = window.pageYOffset; /* Simpan posisi scroll sebelumnya */
+// NAVBAR
+let prevScrollpos = window.pageYOffset /* Simpan posisi scroll sebelumnya */
 
-window.onscroll = function() {
-let currentScrollPos = window.pageYOffset;
-if (prevScrollpos > currentScrollPos) {
-document.getElementById("navbar").style.top = "20px"; /* Scroll ke atas, tampilkan navbar */
-} else {
-document.getElementById("navbar").style.top = "-80px"; /* Scroll ke bawah, sembunyikan navbar */
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('navbar').style.top =
+      '20px' /* Scroll ke atas, tampilkan navbar */
+  } else {
+    document.getElementById('navbar').style.top =
+      '-80px' /* Scroll ke bawah, sembunyikan navbar */
+  }
+  prevScrollpos = currentScrollPos
 }
-prevScrollpos = currentScrollPos;
-}
-
 
 // SECTION FAQ
-document.addEventListener('DOMContentLoaded', function() {
-    var faqCards = document.querySelectorAll('.faq-card');
-  
-    faqCards.forEach(function(card) {
-      card.addEventListener('click', function() {
-        var answer = this.querySelector('.faq-answer');
-        
-        if (this.classList.contains('active')) {
-          this.classList.remove('active');
-          answer.style.maxHeight = null;
-        } else {
-          this.classList.add('active');
-          answer.style.maxHeight = answer.scrollHeight + 'px';
-        }
-      });
-    });
-  });
-  
+document.addEventListener('DOMContentLoaded', function () {
+  var faqCards = document.querySelectorAll('.faq-card')
+
+  faqCards.forEach(function (card) {
+    card.addEventListener('click', function () {
+      var answer = this.querySelector('.faq-answer')
+
+      if (this.classList.contains('active')) {
+        this.classList.remove('active')
+        answer.style.maxHeight = null
+      } else {
+        this.classList.add('active')
+        answer.style.maxHeight = answer.scrollHeight + 'px'
+      }
+    })
+  })
+})
